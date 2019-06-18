23c010843e6acae87a504273db80c3cfc30178af
Daria Lopteva <daria.lopteva@abbyy.com>
2019-06-18 21:16:28 +0300

    fibonacci problem

---
+++ fibonacci.playground/Contents.swift
@@ -0,0 +1,14 @@
+func fibonachi (n: Int) -> Int {
+	guard n >= 0 else {
+		return -1
+	}
+	if n == 0 {
+		return 0
+	}
+	if n == 1 || n == 2 {
+		return 1
+	}
+	return fibonachi(n: n-2) + fibonachi(n: n-1)
+}
+
+
